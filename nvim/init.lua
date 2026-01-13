1043256a875bfa21f7f7c8d0de6ff194c94ba69d
Guilherme Fernandes <gui.fer.sv@gmail.com>
2026-01-13 11:29:06 -0300

    Update keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,9 +74,6 @@ local set = vim.keymap.set
 
 set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
--- set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 set('t', '<C-x>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 --  See `:help wincmd` for a list of all window commands
@@ -110,10 +107,32 @@ set('n', '<leader><tab>c', '<cmd>tabclose<cr>', { desc = 'Close Tab' })
 set('n', '<leader><tab>h', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
 
 -- buffers
-vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
-vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
-vim.keymap.set('n', '<leader><c>', '<cmd>bdelete<cr>', { desc = 'Delete Buffer' })
+set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
+set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+set('n', '<leader>c', '<cmd>bdelete<cr>', { desc = 'Delete Buffer' })
 
+-- quickfix
+set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+set('n', '[q', vim.cmd.cprev, { desc = 'Previous Quickfix' })
+set('n', ']q', vim.cmd.cnext, { desc = 'Next Quickfix' })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  return function()
+    vim.diagnostic.jump {
+      count = (next and 1 or -1) * vim.v.count1,
+      severity = severity and vim.diagnostic.severity[severity] or nil,
+      float = true,
+    }
+  end
+end
+set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Line Diagnostics' })
+set('n', ']d', diagnostic_goto(true), { desc = 'Next Diagnostic' })
+set('n', '[d', diagnostic_goto(false), { desc = 'Prev Diagnostic' })
+set('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
+set('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
+set('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
+set('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
