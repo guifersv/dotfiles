0d3318f6f0b92c079962546e1c97850380e48215
Guilherme Fernandes <gui.fer.sv@gmail.com>
2026-01-13 19:08:08 -0300

    Some plugins

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -66,3 +66,27 @@ set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer
 set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
 set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
 set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+
+set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>")
+set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>")
+set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>")
+
+set({ 'n', 'x', 'o' }, 's', function()
+  require('flash').jump()
+end, { desc = 'Flash' })
+
+set({ 'n', 'x', 'o' }, 'S', function()
+  require('flash').treesitter()
+end, { desc = 'Flash Treesitter' })
+
+set('o', 'r', function()
+  require('flash').remote()
+end, { desc = 'Remote Flash' })
+
+set({ 'o', 'x' }, 'R', function()
+  require('flash').treesitter_search()
+end, { desc = 'Treesitter Search' })
+
+set('c', '<C-s>', function()
+  require('flash').toggle()
+end, { desc = 'Toggle Flash Search' })
--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -4,4 +4,38 @@ return {
     priority = 1000,
     config = true,
   },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  {
+    'chrisgrieser/nvim-spider',
+    lazy = true,
+  },
+  {
+    'folke/flash.nvim',
+    lazy = true,
+  },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  },
 }
